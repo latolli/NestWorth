44a2fb4ed2164199a863253b1a13f6fe7a502c64
FetaJuusto <55877751+latolli@users.noreply.github.com>
2026-09-06 10:03:34 +0300

    Update version and android manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "nest.worth"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.1.1"
+        versionCode = 5
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
